ebb6fd79d95d582333ab7e1764f8dc135cd86466
diegobejar2004 <diegobejar2004@gmail.com>
2025-06-04 13:31:43 -0600

    Change file: config.ts, config to release db

--- core/data/domain/config.ts
+++ core/data/domain/config.ts
@@ -1,8 +1,8 @@
 import { ConnectionOptions } from "mysql2";
 
 export const config: ConnectionOptions = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Nanami04?',
+    host: 'foodnote-db.csb8qe9sl5is.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'foodnote-db25',
     database: 'foodnote',
 }
